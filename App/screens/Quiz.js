cbec6c8ae7d02f4f26304dfe72bfcd62c71b43bd
8989Rahul <daghalerahul0@gmail.com>
2020-06-21 15:48:12 +0530

    quiz component created and wrote some code

--- App/screens/Quiz.js
+++ App/screens/Quiz.js
@@ -1,18 +1,84 @@
 import React from "react";
-import { View, StyleSheet, StatusBar } from "react-native";
+import { View, StyleSheet, StatusBar, Text, SafeAreaView } from "react-native";
+import TEMP_QUESTION from "../data/westerns";
+import { Button, ButtonContainer } from "../components/Button";
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#36B1F0",
     flex: 1,
+    paddingHorizontal: 20,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 25,
+    fontWeight: "600",
+    textAlign: "center",
+    letterSpacing: -0.02,
+  },
+  safeArea: {
+    flex: 1,
+    marginTop: 100,
+    justifyContent: "space-between",
   },
 });
 
 class Quiz extends React.Component {
+  state = {
+    correctCount: 0,
+    totalCount: TEMP_QUESTION?.length,
+    activeQuestionIndex: 0,
+  };
+
+  answer = (correct) => {
+    this.setState(
+      (state) => {
+        const nextState = {};
+        if (correct) {
+          nextState.correctCount = state.correctCount + 1;
+        }
+        return nextState;
+      },
+      () => this.nextQuestion()
+    );
+  };
+
+  nextQuestion = () => {
+    this.setState((state) => {
+      let nextIndex = state.activeQuestionIndex + 1;
+
+      if (nextIndex >= state.totalCount) {
+        nextIndex = 0;
+      }
+      return {
+        activeQuestionIndex: nextIndex,
+      };
+    });
+  };
+
   render() {
+    const question = TEMP_QUESTION[this.state.activeQuestionIndex];
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
+        <SafeAreaView style={styles.safeArea}>
+          <View>
+            <Text style={styles.text}>{question.question}</Text>
+            <ButtonContainer>
+              {question?.answers?.map((answer) => (
+                <Button
+                  key={answer?.id}
+                  text={answer?.text}
+                  onPress={() => this.answer(answer?.correct)}
+                />
+              ))}
+            </ButtonContainer>
+          </View>
+
+          <Text style={styles.text}>
+            {`${this.state.correctCount}/${this.state.totalCount}`}
+          </Text>
+        </SafeAreaView>
       </View>
     );
   }
